0b05678b7d6d72dde8501f07a6fc991348b390c8
Christopher Wirt <cwirt@purdue.edu>
2015-07-22 11:57:12 -0400

    Allows for manual testing

---
+++ ManualTesting/TestProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048 {
+    class TestProgram {
+        static void Main(string[] args) {
+            Game game = new Game(4);
+            game.DisplayBoard();
+            while (game.Running) {
+                try {
+                    game.MakeMove(int.Parse(Console.ReadLine()));
+                    game.DisplayBoard();
+                } catch {
+                    Console.WriteLine("Nope..");
+                }
+            }
+            Console.WriteLine("Number of valid moves made: {0}", game.NumberOfMoves);
+            Console.WriteLine("Final Score: {0}!", game.TotalScore);
+            Console.WriteLine("Largest tile: {0}!", game.CurrentMax);
+            Console.ReadLine();
+        }
+    }
+}
